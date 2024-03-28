8eaae1ce93ae32356195f190ef846b6a65f42c37
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2024-03-28 20:18:47 +0100

    Finish adding proxy settings for macos

--- modules/proxy_macOS.py
+++ modules/proxy_macOS.py
@@ -94,56 +94,71 @@ def fill_in_ip():
     try:
         # Get HTTP proxy settings
         http_result = subprocess.check_output(
-            ["networksetup", "-getwebproxy", "Wi-Fi"], encoding="utf-8"
-        )
-
-        # Extract IP from HTTP settings
+            ["networksetup", "-getwebproxy", "Wi-Fi"], encoding="utf-8")
         http_ip = next((line.split(":")[1].strip(
         ) for line in http_result.splitlines() if "Server:" in line), "0.0.0.0")
 
-        if http_ip == "0.0.0.0":
-            return http_ip
-
         # Get HTTPS proxy settings for comparison
         https_result = subprocess.check_output(
-            ["networksetup", "-getsecurewebproxy", "Wi-Fi"], encoding="utf-8"
-        )
+            ["networksetup", "-getsecurewebproxy", "Wi-Fi"], encoding="utf-8")
         https_ip = next((line.split(":")[1].strip(
         ) for line in https_result.splitlines() if "Server:" in line), "0.0.0.0")
 
         # If HTTP and HTTPS proxy IPs differ, update HTTPS proxy to match HTTP
         if http_ip != https_ip:
             subprocess.check_call(
-                ["networksetup", "-setsecurewebproxy", "Wi-Fi", http_ip, "80"]
-            )
-            print(f"Updated HTTPS proxy to match HTTP proxy: {
-                  https_ip} -> {http_ip}")
+                ["networksetup", "-setsecurewebproxy", "Wi-Fi", http_ip, "8080"])
+            logger.info(
+                f"Updated HTTPS proxy IP to match HTTP proxy IP: {https_ip} -> {http_ip}")
 
         return http_ip
     except subprocess.CalledProcessError as e:
-        print(f"Failed to retrieve or set proxy settings: {e}")
+        logger.error(f"Failed to retrieve or set proxy settings: {e}")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        logger.error(f"An unexpected error occurred: {e}")
 
     return "0.0.0.0"
 
 
 def fill_in_port():
     """
-    Retrieves the current proxy port.
-    If no port address has been set, it returns '8080'.
+    Retrieves the current HTTP proxy Port address. If the HTTPS proxy address
+    is different, it sets the HTTPS Port to match the HTTP proxy's Port address.
+    Returns '0.0.0.0' if no HTTP proxy is set.
 
     Returns:
         str: The current proxy port or '8080' if unset.
     """
     # networksetup -getwebproxy "Wi-Fi"
-
     try:
-        ip_address = ""
-        return ip_address
+        # Get HTTP proxy settings
+        http_result = subprocess.check_output(
+            ["networksetup", "-getwebproxy", "Wi-Fi"], encoding="utf-8")
+        http_ip = next((line.split(":")[1].strip(
+        ) for line in http_result.splitlines() if "Server:" in line), "0.0.0.0")
+        http_port = next((line.split(":")[1].strip(
+        ) for line in http_result.splitlines() if "Port:" in line), "8080")
+
+        # Get HTTPS proxy settings for comparison
+        https_result = subprocess.check_output(
+            ["networksetup", "-getsecurewebproxy", "Wi-Fi"], encoding="utf-8")
+        https_port = next((line.split(":")[1].strip(
+        ) for line in https_result.splitlines() if "Port:" in line), "8080")
+
+        # If HTTP and HTTPS proxy IPs differ, update HTTPS proxy to match HTTP
+        if http_port != https_port:
+            subprocess.check_call(
+                ["networksetup", "-setsecurewebproxy", "Wi-Fi", http_ip, "8080"])
+            logger.info(
+                f"Updated HTTPS proxy to match HTTP proxy: {https_port} -> {http_port}")
+
+        return http_port
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to retrieve or set proxy settings: {e}")
     except Exception as e:
-        print(e)
-        return "8080"
+        logger.error(f"An unexpected error occurred: {e}")
+
+    return "8080"
 
 
 def status_check():
@@ -153,14 +168,38 @@ def status_check():
     Returns:
         bool: True if the proxy is enabled, False otherwise.
     """
-    # networksetup -getwebproxy "Wi-Fi"
 
-    if 1 == 1:
-        logger.info('Proxy is currently active')
-        return True
-    else:
-        logger.info('Proxy is currently inactive')
-        return False
+    try:
+        # Get HTTP proxy settings
+        http_result = subprocess.check_output(
+            ["networksetup", "-getwebproxy", "Wi-Fi"], encoding="utf-8")
+        http_enabled = next((line.split(":")[1].strip(
+        ) for line in http_result.splitlines() if "Enabled:" in line), "No")
+
+        # Get HTTPS proxy settings for comparison
+        https_result = subprocess.check_output(
+            ["networksetup", "-getsecurewebproxy", "Wi-Fi"], encoding="utf-8")
+        https_enabled = next((line.split(":")[1].strip(
+        ) for line in https_result.splitlines() if "Enabled:" in line), "No")
+
+        if http_enabled != https_enabled:
+            subprocess.check_call(
+                ["networksetup", "-setsecurewebproxystate", "Wi-Fi", http_enabled])
+            logger.info(
+                f"Updated HTTPS proxy to match HTTP proxy: {https_enabled} -> {http_enabled}")
+
+        if http_enabled == "Yes":
+            logger.info('Proxy is currently active')
+            return True
+        else:
+            logger.info('Proxy is currently inactive')
+
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to retrieve or set proxy settings: {e}")
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
+
+    return False
 
 
 def server_check():
@@ -171,15 +210,39 @@ def server_check():
     Returns:
         str: The current proxy server address or a placeholder if initially unset.
     """
-    # networksetup -getwebproxy "Wi-Fi"
-
     try:
-        value = ""
+        # Check the current proxy settings
+        check_result = subprocess.check_output(
+            ["networksetup", "-getwebproxy", "Wi-Fi"], encoding="utf-8")
 
-        logger.info(f"Current Proxy Server: {value}")
-        return value
+        server = "0.0.0.0"
+        port = "0"
 
+        # Parse the command output for Server and Port
+        for line in check_result.splitlines():
+            if "Server:" in line:
+                server = line.split(":")[1].strip()
+            elif "Port:" in line:
+                port = line.split(":")[1].strip()
+
+        # Check if the proxy server is not set or set to the default placeholder
+        if server == "0.0.0.0" or not server:
+            logger.warning("No Proxy Server found or set to default!")
+            # Proxy is not properly set, proceed to set the default values
+            subprocess.check_output(
+                ["networksetup", "-setwebproxy", "Wi-Fi", "0.0.0.0", "0"], encoding="utf-8")
+            logger.info("Set the HTTP proxy server to 0.0.0.0:0")
+            subprocess.check_output(
+                ["networksetup", "-setsecurewebproxy", "Wi-Fi", "0.0.0.0", "0"], encoding="utf-8")
+            logger.info("Set the HTTPS proxy server to 0.0.0.0:0")
+            return "0.0.0.0:0"
+        else:
+            # Proxy is properly set, log the server address
+            logger.info(f"Current Proxy Server: {server}:{port}")
+            return f"{server}:{port}"
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to retrieve or set proxy settings: {e}")
     except Exception as e:
-        logger.error(
-            f'An unexpected error occurred while checking: {e}')
-        return "Error"
+        logger.error(f"An unexpected error occurred: {e}")
+
+    return "0.0.0.0:0"
