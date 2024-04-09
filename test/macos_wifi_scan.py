f657e51249d1ac35dfbaa331ac6b4998e66b4d7b
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2024-04-09 21:43:33 +0200

    Refactor logging configuration in macos_wifi_scan.py

--- test/macos_wifi_scan.py
+++ test/macos_wifi_scan.py
@@ -8,18 +8,13 @@ location_manager.requestWhenInUseAuthorization()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-fh = logging.FileHandler('/Users/xpeng/Downloads/logging.log')
-fh.setLevel(logging.DEBUG)
-
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
-logger.addHandler(fh)
 logger.addHandler(ch)
 
 if __name__ == '__main__':
@@ -28,8 +23,8 @@ if __name__ == '__main__':
                     bundle_path=bundle_path,
                     module_globals=globals())
     iface = CWInterface.interface()
-    logger.info(str(iface.interfaceName()))
-    logger.info(iface.ssid())
+    print(
+        f"Interface {str(iface.interfaceName())} - Connected to: {iface.ssid()}\n")
     networks, error = iface.scanForNetworksWithSSID_error_(None, None)
     for network in networks:
         logger.info(network.ssid())
